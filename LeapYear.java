772c926d92f7fd0b5bc3d15acc0f77c5ae6e3062
Satyajit Barik <75263555+Satya2234@users.noreply.github.com>
2022-09-28 19:16:49 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,31 @@
+//Java Program to Check Leap Year
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    // year to be checked
+    int year = {Custom Input}; //Type Your Year
+    boolean leap = false;
+
+    // if the year is divided by 4
+    if (year % 4 == 0) {
+
+      // if the year is century
+      if (year % 100 == 0) {
+
+        // if year is divided by 400
+        // then it is a leap year
+        if (year % 400 == 0)
+          leap = true;
+        else
+          leap = false;
+      }
+      
+      // if the year is not century
+      else
+        leap = true;
+    }
+    
+    else
+      leap = false;
